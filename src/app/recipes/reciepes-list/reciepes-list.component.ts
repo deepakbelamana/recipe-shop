abbb1faed7f350916d6a19099f6c5b393381857d
deepak belamana <saideepak627@gmail.com>
2024-03-20 12:31:58 +0530

    models & component data added

--- src/app/recipes/reciepes-list/reciepes-list.component.ts
+++ src/app/recipes/reciepes-list/reciepes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.mode';
 
 @Component({
   selector: 'app-reciepes-list',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './reciepes-list.component.css'
 })
 export class ReciepesListComponent {
-
+  recipes : Recipe[] = [
+    new Recipe("Test Recipe", "this is simply a test", "https://dcassetcdn.com/design_img/10150/1680/1680_298517_10150_image.jpg")
+  ]
 }

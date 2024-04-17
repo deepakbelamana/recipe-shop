092c2eb9e4c9c4bbe36db3dc7275deb1f685d2d3
deepak <saideepak627@gmail.com>
2024-04-17 12:34:50 +0530

    added navigation to the app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/recipe', pathMatch:'full' },
+  {
+    path: 'shopping-list', component: ShoppingListComponent
+  },
+  {
+    path: 'recipe', component: RecipesComponent
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,7 +29,7 @@ import { DropDownDirective } from './shared/directives/drop-down.directive';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
   ],
   providers: [
     provideClientHydration()
